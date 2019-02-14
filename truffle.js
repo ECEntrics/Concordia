fb5f2a7fb437b1c66e5693f8041f60f6a9255c52
Ezerous <ezerous@gmail.com>
2019-02-14 16:23:12 +0200

    Stayin alive

--- truffle.js
+++ truffle.js
@@ -1,6 +1,7 @@
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
+    contracts_build_directory: path.join(__dirname, "src/build/contracts"),
     networks: {
         development: {
             host: "localhost",
@@ -8,10 +9,15 @@ module.exports = {
             network_id: "*" // Match any network id
         }
     },
-    solc: {
-        optimizer: {
-            enabled: true,
-            runs: 500
+    compilers:{
+        solc: {
+            version: "0.4.25",
+            settings:{
+                optimizer: {
+                    enabled: true,
+                    runs: 500
+                }
+            }
         }
     }
 };
