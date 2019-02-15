929490bfe563de0a91ffb1b7c91a84ed944891ce
Ezerous <ezerous@gmail.com>
2019-02-15 13:12:59 +0200

    Tiny hotfix

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
