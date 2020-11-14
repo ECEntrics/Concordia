207ce0e281bd04521146efe54a6dc943dc58aca1
Apostolof <apotwohd@gmail.com>
2020-11-14 18:00:43 +0200

    Remove drizzle and breeze context

--- packages/concordia-app/src/index.jsx
+++ packages/concordia-app/src/index.jsx
@@ -1,24 +1,14 @@
 import './utils/wdyr';
 import React, { Suspense } from 'react';
 import { render } from 'react-dom';
-import { Drizzle } from '@ezerous/drizzle';
-import { Breeze } from '@ezerous/breeze';
 import App from './App';
 import store from './redux/store';
-import AppContext from './components/AppContext';
-import drizzleOptions from './options/drizzleOptions';
-import breezeOptions from './options/breezeOptions';
 import * as serviceWorker from './utils/serviceWorker';
 import LoadingScreen from './components/LoadingScreen';
 
-const drizzle = new Drizzle(drizzleOptions, store);
-const breeze = new Breeze(breezeOptions, store);
-
 render(
     <Suspense fallback={<LoadingScreen />}>
-        <AppContext.Provider drizzle={drizzle} breeze={breeze}>
-            <App store={store} />
-        </AppContext.Provider>
+        <App store={store} />
     </Suspense>,
     document.getElementById('root'),
 );
--- packages/concordia-app/src/redux/store.js
+++ packages/concordia-app/src/redux/store.js
@@ -1,11 +1,14 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { drizzleReducers, drizzleMiddlewares, generateContractsInitialState } from '@ezerous/drizzle';
-import { breezeReducers } from '@ezerous/breeze';
+import {
+  drizzleReducers, drizzleMiddlewares, generateContractsInitialState, Drizzle,
+} from '@ezerous/drizzle';
+import { Breeze, breezeReducers } from '@ezerous/breeze';
 import createSagaMiddleware from 'redux-saga';
 import userReducer from './reducers/userReducer';
 import rootSaga from './sagas/rootSaga';
 import drizzleOptions from '../options/drizzleOptions';
 import peerDbReplicationReducer from './reducers/peerDbReplicationReducer';
+import breezeOptions from '../options/breezeOptions';
 
 const initialState = {
   contracts: generateContractsInitialState(drizzleOptions),
@@ -23,6 +26,9 @@ const store = configureStore({
   }).concat(drizzleMiddlewares).concat(sagaMiddleware),
   preloadedState: initialState,
 });
+
+export const drizzle = new Drizzle(drizzleOptions, store);
+export const breeze = new Breeze(breezeOptions, store);
 
 sagaMiddleware.run(rootSaga);
 export default store;
