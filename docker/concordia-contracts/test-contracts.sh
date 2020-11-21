790591035885314ad6d44f466d94093021c6f69a
Apostolof <apotwohd@gmail.com>
2020-11-21 22:59:18 +0200

    Minor improvement for contracts test shell script

--- docker/concordia-contracts/test-contracts.sh
+++ docker/concordia-contracts/test-contracts.sh
@@ -5,13 +5,8 @@ yarn _eslint -f html -o /usr/test-reports/concordia-contracts-eslint.html --no-c
   (yarn test >/usr/test-reports/concordia-contracts-truffle-tests.report)
 
 if [ $? -eq 0 ]; then
-  # Tests ran successfully
-  if [ -n "${CI}" ]; then
-    # This is a ci run
-    # Create a pass file to alert the ci about test success
-    # mkdir /usr/test-results && touch /usr/test-results/pass
-    exit 0
-  fi
+  echo "TESTS RAN SUCCESSFULLY!"
+  exit 0
 else
   echo "SOME TESTS FAILED!"
   exit 1
