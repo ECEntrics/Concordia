cd5bd0cd944a3155f9ffe7aff47370ad601745b8
apostolof <apotwohd@gmail.com>
2021-02-07 22:58:52 +0200

    fix: copy shared package in docker images

--- docker/concordia-contracts/migrate.sh
+++ docker/concordia-contracts/migrate.sh
@@ -3,4 +3,4 @@
 export CHAIN_HOST="$DEPLOY_CHAIN_HOST"
 export CHAIN_PORT="$DEPLOY_CHAIN_PORT"
 
-cd /usr/src/concordia/packages/concordia-contracts && yarn _migrate --network "${MIGRATE_NETWORK}" --reset
+cd /usr/src/concordia/packages/concordia-contracts && yarn _migrate --network "${MIGRATE_NETWORK}" --reset && yarn upload
