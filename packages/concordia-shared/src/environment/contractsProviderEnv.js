9a26e6248adfcfb974a6e4bd0c0052d78ef6fb7a
apostolof <apotwohd@gmail.com>
2021-02-13 16:25:36 +0200

    refactor: add support for runtime environment variable in built app

--- packages/concordia-shared/src/environment/contractsProviderEnv.js
+++ packages/concordia-shared/src/environment/contractsProviderEnv.js
@@ -1,10 +1,16 @@
 // Depending on the package user (app in contrast to any of the other packages) the env var names should either include
 // the REACT_APP_ prefix or not
+
+const runtimeEnv = typeof window !== 'undefined' && window.runtimeEnv;
+
 const contractsProviderHostEnv = process.env.REACT_APP_CONTRACTS_PROVIDER_HOST
+    || (runtimeEnv && runtimeEnv.REACT_APP_CONTRACTS_PROVIDER_HOST)
     || process.env.CONTRACTS_PROVIDER_HOST;
 const contractsProviderPortEnv = process.env.REACT_APP_CONTRACTS_PROVIDER_PORT
+    || (runtimeEnv && runtimeEnv.REACT_APP_CONTRACTS_PROVIDER_PORT)
     || process.env.CONTRACTS_PROVIDER_PORT;
 const contractsVersionHashEnv = process.env.REACT_APP_CONTRACTS_VERSION_HASH
+    || (runtimeEnv && runtimeEnv.REACT_APP_CONTRACTS_VERSION_HASH)
     || process.env.CONTRACTS_VERSION_HASH;
 
 module.exports = {
--- packages/concordia-shared/src/environment/rendezvousEnv.js
+++ packages/concordia-shared/src/environment/rendezvousEnv.js
@@ -1,7 +1,14 @@
 // Depending on the package user (app in contrast to any of the other packages) the env var names should either include
 // the REACT_APP_ prefix or not
-const rendezvousHostEnv = process.env.REACT_APP_RENDEZVOUS_HOST || process.env.RENDEZVOUS_HOST;
-const rendezvousPortEnv = process.env.REACT_APP_RENDEZVOUS_PORT || process.env.RENDEZVOUS_PORT;
+
+const runtimeEnv = typeof window !== 'undefined' && window.runtimeEnv;
+
+const rendezvousHostEnv = process.env.REACT_APP_RENDEZVOUS_HOST
+    || (runtimeEnv && runtimeEnv.REACT_APP_RENDEZVOUS_HOST)
+    || process.env.RENDEZVOUS_HOST;
+const rendezvousPortEnv = process.env.REACT_APP_RENDEZVOUS_PORT
+    || (runtimeEnv && runtimeEnv.REACT_APP_RENDEZVOUS_PORT)
+    || process.env.RENDEZVOUS_PORT;
 
 module.exports = {
   rendezvousHostEnv,
