40b3180a47bc15a06f786830985627c18b301a37
apostolof <apotwohd@gmail.com>
2021-02-17 20:05:35 +0200

    refactor: use env var for ipfs repo path

--- packages/concordia-pinner/src/options/ipfsOptions.js
+++ packages/concordia-pinner/src/options/ipfsOptions.js
@@ -1,30 +1,36 @@
 import getLibp2pBundle from './libp2pBundle';
 import { getSwarmAddresses } from '../utils/ipfsUtils';
 import { logger } from '../utils/logger';
+import { IPFS_DIRECTORY_DEFAULT } from '../constants';
 
 const getIpfsOptions = async () => getSwarmAddresses()
   .then((swarmAddresses) => {
     logger.info(`Swarm addresses used: ${swarmAddresses.join(', ')}`);
     return swarmAddresses;
   })
-  .then((swarmAddresses) => ({
-    repo: 'ipfs',
-    config: {
-      Profile: 'server',
-      Addresses: {
-        Swarm: swarmAddresses,
+  .then((swarmAddresses) => {
+    const IPFS_DIRECTORY = process.env.IPFS_DIRECTORY || IPFS_DIRECTORY_DEFAULT;
+    logger.info(`Setting up IPFS in repo: ${IPFS_DIRECTORY}`);
+
+    return ({
+      repo: IPFS_DIRECTORY,
+      config: {
+        Profile: 'server',
+        Addresses: {
+          Swarm: swarmAddresses,
+        },
       },
-    },
-    libp2p: getLibp2pBundle(swarmAddresses),
-    EXPERIMENTAL: {
-      pubsub: true,
-    },
-    preload: {
-      enabled: false,
-    },
-    init: {
-      emptyRepo: true,
-    },
-  }));
+      libp2p: getLibp2pBundle(swarmAddresses),
+      EXPERIMENTAL: {
+        pubsub: true,
+      },
+      preload: {
+        enabled: false,
+      },
+      init: {
+        emptyRepo: true,
+      },
+    });
+  });
 
 export default getIpfsOptions;
