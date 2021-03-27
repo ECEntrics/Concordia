1e1c1cd3afec31a964bb5417220e7d49055de184
apostolof <apotwohd@gmail.com>
2021-03-27 21:29:06 +0200

    fix: fix tab titles intl

--- packages/concordia-app/src/constants/polls/PollTabs.js
+++ packages/concordia-app/src/constants/polls/PollTabs.js
@@ -1,11 +1,11 @@
 export const VOTE_TAB = {
   id: 'vote-tab',
-  intl_display_name_id: 'topic.poll.vote.tab.title',
+  intl_display_name_id: 'topic.poll.tab.vote.title',
 };
 
 export const GRAPH_TAB = {
   id: 'graph-tab',
-  intl_display_name_id: 'topic.poll.graph.tab.title',
+  intl_display_name_id: 'topic.poll.tab.graph.title',
 };
 
 const pollTabs = [
