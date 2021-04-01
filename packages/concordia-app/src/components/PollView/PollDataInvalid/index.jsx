5c850834d7e1812f3b4398b151f66b26eb931681
apostolof <apotwohd@gmail.com>
2021-04-01 18:32:33 +0300

    refactor: replace hardcoded strings with intl equivalents

--- packages/concordia-app/src/components/PollView/PollDataInvalid/index.jsx
+++ packages/concordia-app/src/components/PollView/PollDataInvalid/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Container, Header, Icon } from 'semantic-ui-react';
+import { useTranslation } from 'react-i18next';
 
-const PollDataInvalid = () => (
-    <Container id="topic-poll-data-invalid-container">
-        <Header as="h3">
-            <Icon name="warning sign" color="red" />
-            <Header.Content>
-                This topic has a poll but the data are untrusted!
-                <Header.Subheader>The poll data downloaded from the poster have been tampered with.</Header.Subheader>
-            </Header.Content>
-        </Header>
-    </Container>
-);
+const PollDataInvalid = () => {
+  const { t } = useTranslation();
+
+  return (
+      <Container id="topic-poll-data-invalid-container" textAlign="center">
+          <Header as="h3" icon textAlign="center">
+              <Icon name="warning sign" color="red" />
+              <Header.Content>
+                  {t('topic.poll.invalid.data.header')}
+                  <Header.Subheader>
+                      {t('topic.poll.invalid.data.sub.header')}
+                  </Header.Subheader>
+              </Header.Content>
+          </Header>
+      </Container>
+  );
+};
 
 export default PollDataInvalid;
